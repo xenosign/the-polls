f130626de7d39bae11f890aa55b24e54b037fd7c
kdtTetz <xenosign@naver.com>
2022-12-26 00:39:34 +0900

    Update main.jsx

--- src/pages/main.jsx
+++ src/pages/main.jsx
@@ -1,22 +1,5 @@
 import React from "react";
 
-useEffect(() => {
-  setGardenEmail(emailLocation);
-
-  async function fetchData() {
-    const result = await axios(`${API.SNOWMAN}${gardenEmail}`);
-    if (result) {
-      setData(result.data.snowmans);
-      setNickname(result.data.nickname);
-    }
-  }
-
-  if (gardenEmail !== undefined) {
-    fetchData();
-    setConnect(true);
-  }
-}, [gardenEmail]);
-
 export default function main() {
   return <div>main</div>;
 }
